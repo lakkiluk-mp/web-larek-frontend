1c137b3113f12e564922524324bbdd698b1f5863
Maxim Postika <lakkiluk.m@gmail.com>
2024-02-17 16:34:58 +0300

    types
    odified:   README.md

--- src/components/LarekAPI.ts
+++ src/components/LarekAPI.ts
@@ -1,11 +1,9 @@
 import { Api, ApiListResponse } from './base/api';
-import {IOrder, IOrderResult, ILot, LotUpdate, IBid} from "../types";
+import {IOrder, IOrderResult, ILot,} from "../types";
 
 export interface IAuctionAPI {
     getLotList: () => Promise<ILot[]>;
     getLotItem: (id: string) => Promise<ILot>;
-    getLotUpdate: (id: string) => Promise<LotUpdate>;
-    placeBid(id: string, bid: IBid): Promise<LotUpdate>;
     orderLots: (order: IOrder) => Promise<IOrderResult>;
 }
 
@@ -26,24 +24,12 @@ export class AuctionAPI extends Api implements IAuctionAPI {
         );
     }
 
-    getLotUpdate(id: string): Promise<LotUpdate> {
-        return this.get(`/lot/${id}/_auction`).then(
-            (data: LotUpdate) => data
-        );
-    }
-
     getLotList(): Promise<ILot[]> {
         return this.get('/lot').then((data: ApiListResponse<ILot>) =>
             data.items.map((item) => ({
                 ...item,
                 image: this.cdn + item.image
             }))
-        );
-    }
-
-    placeBid(id: string, bid: IBid): Promise<LotUpdate> {
-        return this.post(`/lot/${id}/_bid`, bid).then(
-            (data: ILot) => data
         );
     }
 
